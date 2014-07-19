6107d3d587a3c378eafc05d264ddb014814ec57c
Zac Tolley <zac@thetolleys.com>
2014-07-19 14:29:10 +0100

    Initial Commit

---
+++ ParentCab/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  ParentCab
+//
+//  Created by Zac Tolley on 19/07/2014.
+//  Copyright (c) 2014 Zac Tolley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ ParentCab/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ParentCab
+//
+//  Created by Zac Tolley on 19/07/2014.
+//  Copyright (c) 2014 Zac Tolley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
